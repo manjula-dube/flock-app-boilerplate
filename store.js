8e8e587964cd4f5ed973abd44fd482c7255a6adc
Manjula Subhashchandra Dube <dube.manjula668@gmail.com>
2017-02-12 17:33:50 +0530

    intial boilerplate for flockos

--- store.js
+++ store.js
@@ -42,35 +42,3 @@ exports.saveToken = function (userId, token) {
     db.users[userId] = token;
 };
 
-exports.peekAlarm = function () {
-    if (db.alarms.length > 0) {
-        return db.alarms[0];
-    } else {
-        return null;
-    }
-};
-
-exports.removeAlarm = function (alarm) {
-    var index = db.alarms.indexOf(alarm);
-    if (index !== -1) {
-        db.alarms.splice(index, 1);
-    }
-};
-
-exports.addAlarm = function (alarm) {
-    var alarms = db.alarms;
-    var insertAt = Math.max(0, alarms.findIndex(function (x) {
-        return alarm.time < x.time;
-    }));
-    alarms.splice(insertAt, 0, alarm);
-};
-
-exports.userAlarms = function (userId) {
-    return db.alarms.filter(function (alarm) {
-        return alarm.userId === userId;
-    });
-};
-
-exports.allAlarms = function () {
-    return db.alarms;
-};
